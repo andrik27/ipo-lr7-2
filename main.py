89a345078f082caf4c7bc71e3db1fa187575bfb7
Endru <88160476+andrik27@users.noreply.github.com>
2024-11-21 09:33:34 +0300

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
 
-number = int(input("Введите номер квалификации: "))
+number = str(input("Введите номер квалификации: "))
 find = False
 
-with open("dump.json", 'r', encoding='utf-8') as file: 
+with open("D:/profiles/krachkA_82/Desktop/ipo-lr7-2-main/dump.json", 'r', encoding='utf-8') as file: 
     data = json.load(file) 
     for skill in data:
         if skill.get("model") == "data.skill":
-            if skill["fields"].get("specialty") == number: 
+            if skill["fields"].get("code") == number: 
                 skill_code = skill["fields"].get("code")
                 skill_title = skill["fields"].get("title")
                 find = True
@@ -15,7 +15,7 @@ with open("dump.json", 'r', encoding='utf-8') as file:
                 for profession in data:
                     if profession.get("model") == "data.specialty":
                         specialty_code = profession["fields"].get("code")
-                        if specialty_code in skill_code:  
+                        if skill_code in specialty_code:  
                             specialty_title = profession["fields"].get("title")
                             specialty_educational = profession["fields"].get("c_type")
                             
